7d58ad60b235838b08a8337be55d68536527c837
Valentin Brosch <brosch@sternsinger.de>
2020-02-11 20:52:43 +0100

    inc batch size

--- scripts/cifar10_macro_search.sh
+++ scripts/cifar10_macro_search.sh
@@ -8,7 +8,7 @@ python src/cifar10/main.py \
   --reset_output_dir \
   --data_path="data/cifar10" \
   --output_dir="outputs" \
-  --batch_size=1 \
+  --batch_size=128 \
   --num_epochs=310 \
   --log_every=50 \
   --eval_every_epochs=1 \
